73b1acd61de27c4e9268cd24cd8a8b267bd36508
roman.shhh <55157344+roman-shhh@users.noreply.github.com>
2025-03-18 06:13:08 -0400

    User Management with REACT front-end

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,11 +7,15 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
+    # path for registration
+    path(route='register', view=views.registration, name='register'),
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
     path(route='login', view=views.login_user, name='login'),
-    path(route="logout", view=views.logout_request, name="logout"),
+    
+    # path for logout
+    path(route='logout',  view=views.logout_request, name="logout"),
+    
     # path for dealer reviews view
 
     # path for add a review view
